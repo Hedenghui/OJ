1ce976580464779ba608c882d518e1e6c4252157
Hedenghui <1143671646@qq.com>
2020-12-02 23:53:26 +0800

    剑指offer oj 包含min函数的栈

---
+++ 2020.12.2.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+#include<stack>
+
+class MinStack {
+public:
+	/** initialize your data structure here. */
+	MinStack() {
+
+	}
+
+	void push(int x) {
+		if (s1.empty() || x <= s2.top())
+		{
+			s2.push(x);
+		}
+		s1.push(x);
+	}
+
+	void pop() {
+		if (s1.top() == s2.top())
+		{
+			s2.pop();
+		}
+		s1.pop();
+	}
+
+	int top() {
+		return s1.top();
+	}
+
+	int min() {
+		return s2.top();
+	}
+	stack<int> s1;
+	stack<int> s2;
+};
+//int main()
+//{
+//	return 0;
+//}
