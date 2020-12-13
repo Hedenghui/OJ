793bd7d5bac9358eb2fa260121f1cfed31f25bbe
Hedenghui <1143671646@qq.com>
2020-12-13 23:41:52 +0800

    剑指offer oj 圆圈中最后剩下的数字

---
+++ 2020.12.13.1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+	int lastRemaining(int n, int m) {
+		int p = 0;
+		for (int i = 2; i <= n; i++)
+		{
+			p = (p + m) % i;
+		}
+		return p;
+	}
+};
+
+
+//int main()
+//{
+//	return 0;
+//}
