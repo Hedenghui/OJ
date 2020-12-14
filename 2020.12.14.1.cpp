1c797d9193b3dcb8323b93c69c51b6279f37d926
Hedenghui <1143671646@qq.com>
2020-12-14 23:39:01 +0800

    剑指offer oj 求1+2+...+n

---
+++ 2020.12.14.1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+	int sumNums(int n) {
+		n && (n += sumNums(n - 1));
+		return n;
+	}
+};
+//int main()
+//{
+//	return 0;
+//}
