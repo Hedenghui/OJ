781e9f11355d5272dd0c15071ba70ded092b0dd0
Hedenghui <1143671646@qq.com>
2021-01-29 22:59:26 +0800

    HOT 31. 下一个排列

---
+++ 2021.1.29.1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+class Solution {
+public:
+	void nextPermutation(vector<int>& nums) {
+		int i = nums.size() - 2;
+		while (i >= 0 && nums[i] >= nums[i + 1])
+			i--;
+
+		if (i >= 0)
+		{
+			int j = nums.size() - 1;
+			while (j >= 0 && nums[j] <= nums[i])
+			{
+				j--;
+			}
+			swap(nums[i], nums[j]);
+		}
+		reverse(nums.begin() + i + 1, nums.end());
+
+	}
+};
+int main()
+{
+	return 0;
+}
